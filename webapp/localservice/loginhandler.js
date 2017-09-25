b625c7497340014cedb1ea18b77e27cb99c9c054
Pavan Athreyas SJ <pavan.athreyas.sj@sap.com>
2017-09-25 21:26:49 +0000

    Change list 1. added commenting to util files 2. Add i18n_en properties file (dummy) 3. Removed control files which are not used

--- webapp/localservice/loginhandler.js
+++ webapp/localservice/loginhandler.js
@@ -3,19 +3,41 @@ sap.ui.define([], function() {
 
 	return {
 
+		/**
+		 * Method to set the reference of model to the TMS Hanlder file
+		 * @memberOf TMS.localService.loginhanlder
+		 * @param {Object} - oAppStateModel - Model instance of the app state
+		 *        {Object} - oAppStateModel - Model instance Main model with master data
+		 */
 		setModelReferences: function(oAppStateModel, oLoginModel) {
 			this.oAppStateModel = oAppStateModel;
 			this.oLoginModel = oLoginModel;
 		},
 
+		/**
+		 * Method to set the User Details to appstate model
+		 * @memberOf TMS.localService.loginhanlder
+		 * @param {Object} - oExistingUser - User details from Master data
+		 */
 		setCurrentUserDetails: function(oExistingUser) {
 			this.oAppStateModel.setProperty("/loggedinuser", oExistingUser);
 		},
 
+		/**
+		 * Method to get the user details from appstate model
+		 * @memberOf TMS.localService.loginhanlder
+		 *  @return {Object} - returns a JSON object with the user details
+		 */
 		getCurrentUserDetails: function() {
 			return this.oAppStateModel.getProperty("/loggedinuser");
 		},
 
+		/**
+		 * Method to get the  user details from master data
+		 * @memberOf TMS.localService.loginhanlder
+		 * @param {string} - sUserId -user id based on login action
+		 * @return {Object} - returns a JSON object with the user details
+		 */
 		getUserById: function(sUserId) {
 			var aUsers = this.oLoginModel.getProperty("/");
 
@@ -25,6 +47,13 @@ sap.ui.define([], function() {
 			return aUsers.find(findUsers);
 		},
 
+		/**
+		 * Method handler to check the user details on submit actions
+		 * @memberOf TMS.localService.loginhanlder
+		 * @param {string} - sUserName -Entered user name
+		 *        {string} - sPassword -Entered password
+		 * @return {string} - returns appropriate message based on user inputs
+		 */
 		authenticateUser: function(sUserName, sPassword) {
 
 			var aUsers = this.oLoginModel.getProperty("/");
@@ -48,6 +77,13 @@ sap.ui.define([], function() {
 				};
 			}
 		},
+
+		/**
+		 * Method handler to check the user details on submit actions
+		 * @memberOf TMS.localService.loginhanlder
+		 * @param {Object} - oNewUser -JSON Object with New user registeration details
+		 * @return {string} - returns appropriate message based on user inputs
+		 */
 
 		registerUser: function(oNewUser) {
 			var aUsers = this.oLoginModel.getProperty("/");
